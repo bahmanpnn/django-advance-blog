228645637bb039578ccd7bd23a5e7d39e330be82
bahmanpnn <bahmanpn@gmail.com>
2023-05-03 21:50:31 +0330

    added usermaneger class for checking user creating and fixed databes problem and user model and add auth_user_model to settings

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -2,19 +2,47 @@ from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager,AbstractBaseUser,PermissionsMixin
 )
+from django.utils.translation import gettext_lazy as _show_message
 
+class UserManager(BaseUserManager):
+    """
+    this method is for control creating object of user model
+    """
+    def create_user(self,email,password,**extra_fields):
+        if not email:
+            raise ValueError(_show_message("the email must be set"))
+        email=self.normalize_email(email)
+        user=self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self,email,password,**extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_active',True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_show_message("superuser must have is_staff==True"))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_show_message("superuser must have is_superuser==True"))
+        
+        return self.create_user(email,password,**extra_fields)
 
 class User(AbstractBaseUser,PermissionsMixin):
-    first_name=models.CharField(max_length=50)
-    email=models.EmailField(max_length=254)
+    # first_name=models.CharField(max_length=50)
+    email=models.EmailField(max_length=254,unique=True)
     is_staff=models.BooleanField(default=False)
+    is_superuser=models.BooleanField(default=False)
     is_active=models.BooleanField(default=True)
+
     created_date=models.DateTimeField(auto_now_add=True)
     updated_date=models.DateTimeField(auto_now=True)
     # is_verified=models.BooleanField(default=False)
 
     USERNAME_FIELD='email'
-    REQUIRED_FIELDS=['email']
+    REQUIRED_FIELDS=[]
 
+    objects=UserManager()
+    
     def __str__(self):
         return self.email
