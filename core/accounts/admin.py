27f31c77273e3ece6fcca216f63a9d0c9ca5d9c6
bahmanpnn <bahmanpn@gmail.com>
2023-05-04 19:25:07 +0330

    added CustomUserAdmin class and set fieldsets and add_fieldsets to add admin user change ro show admin list and detail page of that

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model=User
+    list_display=('email','is_superuser','is_staff','is_active')
+    list_filter=('email','is_superuser','is_staff','is_active')
+    search_fields=('email',)
+    ordering=('email',)
+    fieldsets = (
+        ('Authentication', {
+            "fields": (
+                'email','password'
+            ),
+        }),
+        ('Permissions', {
+            "fields": (
+                'is_staff','is_superuser','is_active'
+            ),
+        }),
+        ('Group Permissions', {
+            "fields": (
+                'groups','user_permissions',
+            ),
+        }),
+        ('Important Date', {
+            "fields": (
+                'last_login',
+            ),
+        })
+    )
+    add_fieldsets=(
+        (None,{
+            'classes':('wide',),
+            'fields':('email','password1','password2','is_staff','is_active','is_superuser')
+        }),
+    )
+    
+
+admin.site.register(User,CustomUserAdmin)
