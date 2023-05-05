421bd71c2dfb026dfd14faf703171cb88b46f2d1
bahmanpnn <bahmanpn@gmail.com>
2023-05-05 21:57:20 +0330

    3.8-added profile model in accounts models to next step connect to user model with signals

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,9 +1,47 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User,Profile
+from django import forms
+from django.forms import ValidationError
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+
+# class UserCreationForm(forms.ModelForm):
+#     '''
+#     a form for creating new users.includes all the required fields, plus a repeated password
+#     '''
+#     password1=forms.CharField(label='password',widget=forms.PasswordInput)
+#     password2=forms.CharField(label='password confirmation',widget=forms.PasswordInput)
+
+#     class Meta:
+#         fields=('email',)
+#         model=User
+
+#     def clean_password2(self):
+#         #check that two passwords match or not
+#         password1=self.clean_data.get('password1')
+#         password2=self.clean_data.get('password2')
+#         if password1 and password2 and password1 !=password2:
+#             raise ValidationError('passwords dont match')
+#         return password2
+
+#     def save(self,commit=True):
+#         #save the provied password in hashed format
+#         user=super().save(commit=False)
+#         user.set_passowrd(self.cleaned_data['password1'])
+#         if commit:
+#             user.save()
+#         return user
+
+# class CustomUserCreateForm(UserCreationForm):
+#     class Meta:
+#         model=User
+#         fields=('email',)
+
 
 class CustomUserAdmin(UserAdmin):
     model=User
+    # add_form=CustomUserCreateForm
+    # add_form=UserCreationForm
     list_display=('email','is_superuser','is_staff','is_active')
     list_filter=('email','is_superuser','is_staff','is_active')
     search_fields=('email',)
@@ -39,3 +77,4 @@ class CustomUserAdmin(UserAdmin):
     
 
 admin.site.register(User,CustomUserAdmin)
+admin.site.register(Profile)
