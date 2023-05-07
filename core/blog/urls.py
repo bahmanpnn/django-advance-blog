76d4eebbe5df2baad9e12a3dde7e5d9a671424f4
bahmanpnn <bahmanpn@gmail.com>
2023-05-07 23:13:37 +0330

    added some templateview and redirectview for test and training

---
+++ core/blog/urls.py
@@ -0,0 +1,19 @@
+
+from django.urls import path
+from .views import *
+from django.views.generic import TemplateView,RedirectView
+
+
+app_name='blog'
+
+urlpatterns = [
+    path('fbv_index',index,name='fbv-index'),
+    path('cbv_index',TemplateView.as_view(template_name="index.html",extra_context={"name":'hamid'}),name='cbv-index'),
+    path('my_cbv_index',IndexView.as_view(),name='my-cbv-index'),
+    path('go-to-github',RedirectView.as_view(url='https://github.com'),name='go-to-github'),
+    path('go-to-cbv-index',RedirectView.as_view(pattern_name="blog:cbv-index"),name='go-to-cbv-index'),
+    path('fbv-go-to-github',github_redirect,name='fbv-index'),
+    path('cbv-go-to-maktabkhoone',RedirectToMaktabkhoone.as_view(),name='redirect-to-maktabkhoone-cbv'),
+    path('cbv-go-to-maktabkhoone-two/<int:pk>',RedirectToMaktabkhooneTwo.as_view(),name='redirect-to-maktabkhoone-cbv-two'),
+
+]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,42 @@
+from typing import Any, Optional
 from django.shortcuts import render
+from django.views.generic.base import TemplateView,RedirectView
+from django.shortcuts import redirect,get_object_or_404
+from .models import Post
 
-# Create your views here.
+def index(request):
+    name='bahman'
+    context={"name":name}
+    return render(request,'index.html',context)
+
+
+
+class IndexView(TemplateView):
+    template_name="index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["name"] ='bahman' 
+        return context
+    
+
+def github_redirect(request):
+    return redirect('https://github.com')
+
+class RedirectToMaktabkhoone(RedirectView):
+    url='https://maktabkhooneh.org'
+    # permanent=False
+    # query_string=False
+    # pattern_name=''
+
+    
+class RedirectToMaktabkhooneTwo(RedirectView):
+    url='https://maktabkhooneh.org'
+    # permanent=False
+    # query_string=False
+    # pattern_name=''
+
+    def get_redirect_url(self, *args: Any, **kwargs: Any):
+        post=get_object_or_404(Post,pk=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
