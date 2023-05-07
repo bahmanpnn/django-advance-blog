68cd4c1aa0ddc394aad4c9d2ab5ccdcb210905a3
bahmanpnn <bahmanpn@gmail.com>
2023-05-07 17:51:44 +0330

    added get_user_model for user in blog app models for post and migration

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,13 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+# from accounts.models import User
+# from django.conf import settings
 
-# Create your models here.
+#getting user model object
+
+User=get_user_model()
 
 class Post(models.Model):
     '''
     this is a class to define posts for blog app
     '''
 
-    auhtor=models.ForeignKey("User", on_delete=models.CASCADE)
+    # auhtor=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # auhtor=models.ForeignKey("User", on_delete=models.CASCADE)
+    auhtor=models.ForeignKey(User, on_delete=models.CASCADE)
     image=models.ImageField(null=True,blank=True)
     title=models.CharField(max_length=250)
     content=models.TextField()
